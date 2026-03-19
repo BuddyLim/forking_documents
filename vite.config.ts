69ca66d88ec4eea40d775c0c97a33aa9de02d2fd
Kuang Tar Lim <buddy.tlimk@gmail.com>
2026-03-19 14:54:35 +0800

    fix: vite config and deployment script

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
-/// <reference types="vitest" />
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/forking-documents/' : '/',
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/forking-documents/' : '/',
   plugins: [react()],
   test: {
     environment: 'jsdom',
     globals: true,
     setupFiles: ['./src/test/setup.ts'],
   },
-})
+}))
